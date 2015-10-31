faed2c6d0d79e9617bc566976434d8576d17df2c
patrickhsanders <patrickhsanders@gmail.com>
2015-10-30 23:09:30 -0400

    Added c and swift

---
+++ twinprimes-c/TwinPrimes/main.c
@@ -0,0 +1,39 @@
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+void printTwinPrimesToN(unsigned long);
+
+int main(int argc, const char * argv[]) {
+    printTwinPrimesToN(1000000);
+    return 0;
+}
+
+void printTwinPrimesToN(unsigned long n){
+    
+    unsigned long *pointer;
+    unsigned long length = 1;
+    pointer = malloc(length * sizeof(unsigned long));
+    if(pointer==NULL){ printf("Error allocating memory"); return; }
+    
+    pointer[0] = 2;
+    
+    for(unsigned long x = 3; x < n; x++){
+        int isPrime = 1;
+        for(unsigned long i = 0; i < length; i++){
+            if(isPrime == 1 && x % pointer[i] == 0)
+            {
+                isPrime = 0;
+            }
+        }
+        if(isPrime){
+            unsigned long *temp = realloc(pointer, (++length * sizeof(unsigned long)));
+            if(temp != NULL) { pointer = temp; } else { return; }
+            pointer[length - 1] = x;
+            if(pointer[length - 1] - pointer[length - 2] == 2){
+                printf("<%lu,%lu>\n",pointer[length - 2],(pointer[length - 1]));
+            }
+        }
+    }
+    free(pointer);
+}
---
+++ twinprimes-swift/TwinPrimes-swift/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  TwinPrimes-swift
+//
+//  Created by Patrick Sanders on 30.10.15.
+//  Copyright © 2015 turntotech. All rights reserved.
+//
+
+import Foundation
+
+func printTwinPrimesToN(n: Int) -> Void {
+    var arrayOfPrimes: [Int] = [2,3]
+    
+    for var i:Int = 4; i < n; i++ {
+        var valueIsPrime:Bool = true;
+        
+        
+        for var j:Int = 0; j < arrayOfPrimes.count; j++ {
+            if(valueIsPrime && i % arrayOfPrimes[j] == 0) {
+                valueIsPrime = false
+            }
+        }
+        
+        if valueIsPrime {
+            arrayOfPrimes.append(i)
+            if arrayOfPrimes[arrayOfPrimes.count - 1] - arrayOfPrimes[arrayOfPrimes.count - 2] == 2 {
+                print("<\(arrayOfPrimes[arrayOfPrimes.count-2]), \(arrayOfPrimes[arrayOfPrimes.count-1])>")
+            }
+        }
+    }
+}
+
+
+printTwinPrimesToN(1000000)
